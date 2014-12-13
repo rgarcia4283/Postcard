7366f2e5f27993e038ac285790ce80af4553400b
Rafael Garcia <rafaelgarcia@MacBook-Pro-de-Rafael.local>
2014-12-13 15:08:51 -0800

    save second

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // add to comment here
     }
 
     override func didReceiveMemoryWarning() {
